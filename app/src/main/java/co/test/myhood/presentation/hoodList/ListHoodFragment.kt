f01850e0b8548969bf0fd910458214576024ff4f
Juan Pablo <juanscigliano@gmail.com>
2020-06-16 18:29:16 +0200

    Added initial navagition to hoodList.

--- app/src/main/java/co/test/myhood/presentation/hoodList/ListHoodFragment.kt
+++ app/src/main/java/co/test/myhood/presentation/hoodList/ListHoodFragment.kt
@@ -1,27 +1,19 @@
-package co.test.myhood.presentation
+package co.test.myhood.presentation.hoodList
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import co.test.myhood.R
 
-class InitialFragment : Fragment() {
-
-
-    private val navController by lazy {
-        findNavController()
-    }
+class ListHoodFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_initial, container, false)
+        return inflater.inflate(R.layout.fragment_list_hood, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        //is user logged?
-        navController.navigate(InitialFragmentDirections.actionInitialFragmentToListHoodFragment())
         super.onViewCreated(view, savedInstanceState)
     }
 }
